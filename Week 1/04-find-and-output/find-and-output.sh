b44a97c1bd49de084a720a03730620821c499fe4
carrordeng <carror1994@gmail.com>
2018-04-07 17:01:26 +0800

    Week1

---
+++ Week 1/04-find-and-output/find-and-output.sh
@@ -0,0 +1,1 @@
+find /etc -name "*.conf" 2> ~/error.txt | sort -o ~/conflist.txt
--- Week 1/05-backup-log-by-crontab/backup.sh
+++ Week 1/05-backup-log-by-crontab/backup.sh
@@ -3,7 +3,7 @@ sudo cron -f &
 cd ~
 crontab -e
 00 3 * * * cd /home/shiyanlou/backup && tar -zcf 'date + \%Y-\%m-\%d'.tar.gz -g metadata /var/log
-或者
+#or
 00 3 * * * cd /home/shiyanlou/backup && tar -zcf /home/shiyanlou/backup/$(date + \%Y-\%m-\%d).tar.gz -g metadata /var/log
-或者
+#or
 00 3 * * * tar -zcf /home/shiyanlou/backup/$(date + \%Y-\%m-\%d).tar.gz -g /home/shiyanlou/backup/metadata /var/log
