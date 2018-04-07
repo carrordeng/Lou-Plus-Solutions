3bc8cba3e386cd68ddf16345b2821758cb1187cb
carrordeng <carror1994@gmail.com>
2018-04-07 17:01:44 +0800

    Week 2

--- Week 2/09-auto-recovery-service-script/check_service.sh
+++ Week 2/09-auto-recovery-service-script/check_service.sh
@@ -55,28 +55,3 @@ crontab -e
  
 
 00 00 * * * /home/shiyanlou/check_service.sh mysql
-
-
-
-
-
-
-
-better approach
-
-#!/bin/bash
-
-sudo service $1 status &> /dev/null
-status=$?
-
-case $status in
-0)
-    echo "is Running";;
-1)
-    echo "Error: Service Not Found" && exit 1;;
-3)
-    echo "Restarting"
-    sudo service $1 start &> /dev/null;;
-*)
-    echo "ERROR" && exit 1
-esac
